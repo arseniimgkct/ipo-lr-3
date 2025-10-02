75cf983421e8f84f0a11f472c4deddf062a9fb46
arseniimgkct <mgkct89172@gmail.com>
2025-10-02 10:36:34 +0300

    fix: validating days for every month

--- task_3/main.py
+++ task_3/main.py
@@ -1,13 +1,29 @@
 day = int(input("Введите день числом: ")) # получаем день
 month = int(input("Введите месяц числом: ")) # получаем месяц
 
+if day <= 0 or day > 31:
+    print("Некорректный день")
+    exit()
+
 if month >= 3 and month < 6: # если месяц в диапазоне от 3 до 6, то это весна
-    print("Весна") # выводим весну
+    if ((month == 3 or month == 5) and day > 31) or (month == 4 and day > 30):
+            print("Некорректный день")
+    else:
+        print("Весна") # выводим весну
 elif month >= 6 and month < 9: # если месяц в диапазоне от 6 до 9, то это лето
-    print("Лето") # выводим лето
+    if ((month == 7 or month == 8) and day > 31) or (month == 6 and day > 30):
+            print("Некорректный день")
+    else:
+        print("Лето") # выводим лето
 elif month >= 9 and month < 12: # если месяц в диапазоне от 9 до 12, то это осень
-    print("Осень") # выводим осень
+    if ((month == 9 or month == 11) and day > 30) or (month == 10 and day > 31):
+        print("Некорректный день")
+    else:
+        print("Осень") # выводим осень
 elif month == 12 or month < 3: # если месяц равен 12 или меньше 3, то это зима
-    print("Зима") # выводим зиму
+    if ((month == 12 or month == 1) and day > 31) or (month == 2 and day > 28):
+        print("Некорректный день")
+    else:
+        print("Зима") # выводим осень
 else: # обрабатываем невалидные данные
     print("Некорректный месяц") # выводим
